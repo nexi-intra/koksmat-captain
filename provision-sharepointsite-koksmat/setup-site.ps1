e34026f3a735574724e410137e5c63eccf5c48fc
Niels Gregers Johansen <niels@timeboxer.dk>
2025-01-05 14:35:34 +0100

    added calculated column linking to github

--- provision-sharepointsite-koksmat/setup-site.ps1
+++ provision-sharepointsite-koksmat/setup-site.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 
 function List($listName) {
-  $repoList = Get-PnPList -Identity $listName
+  $repoList = Get-PnPList -Identity $listName -ErrorAction SilentlyContinue
   write-host "Checking list $listName" -ForegroundColor Green
   if ($null -eq $repoList) {
     write-host "Creating list $listName" -ForegroundColor Yellow
@@ -26,25 +26,44 @@ function ChoiceField($listName, $fieldName, $displayName, $choices) {
   }
 }
 
-try {
-  $site = get-pnpsite
-  List "Repositories"
-  TextField "Repositories" "RepositoryName" "Repository Name"  $true
-  TextField "Repositories" "OrganisationName" "Organisation Name"  $true
-  ChoiceField "Repositories" "RepositoryType" "Repository Type"  @("Public", "Private")
-  TextField "Repositories" "DocRepositoryName" "Documenation Repository Name"  $false
-  TextField "Repositories" "DocOrganisationName" "Documenation Organisation Name"  $false
-  ChoiceField "Repositories" "DocumentationType" "Documentation Type"  @("Main branch", "All branches", "None")
-  
-  
-  
-  
-  
+function CalculatedField($listName, $fieldName, $displayName, $formula) {
+  $field = Get-PnPField -List $listName -Identity $fieldName -ErrorAction SilentlyContinue
+  if ($null -eq $field) {
+    write-host "Creating field $fieldName" -ForegroundColor Yellow
+    $field = Add-PnPField -List $listName -Type Calculated -InternalName $fieldName -DisplayName $displayName -Formula $formula -AddToDefaultView
+  }
+  else {
+    write-host "Updating field $fieldName" -ForegroundColor Yellow
+    Set-PnPField -List $listName -Identity $fieldName -Values @{ Formula = $formula }
+  }
+}
 
-  
-    
+List "Repositories"
+TextField "Repositories" "RepositoryName" "Repository Name"  $true
+TextField "Repositories" "OrganisationName" "Organisation Name"  $true
+ChoiceField "Repositories" "RepositoryType" "Repository Type"  @("Public", "Private")
+TextField "Repositories" "DocRepositoryName" "Documenation Repository Name"  $false
+TextField "Repositories" "DocOrganisationName" "Documenation Organisation Name"  $false
+ChoiceField "Repositories" "DocumentationType" "Documentation Type"  @("Main branch", "All branches", "None")
+CalculatedField "Repositories" "GitHub" "GitHub"  @"
+=CONCATENATE("https://github.com","/",[Organisation Name],"/",[Repository Name])
+"@
+
+
+$json = @'
+{
+  "$schema": "https://developer.microsoft.com/json-schemas/sp/column-formatting.schema.json",
+  "elmType": "a",
+  "attributes": {
+    "href": "@currentField",
+    "target": "_blank"
+  },
+  "txtContent": "@currentField",
+  "style": {
+    "color": "#0072C6",
+    "text-decoration": "underline"
+  }
 }
-catch {
-  Write-Host "Error: $_" -ForegroundColor Red
-  throw "Setup site Failed - Error: $_"
-}
+'@
+
+Set-PnPField -List "Repositories" -Identity "GitHub" -Values @{ CustomFormatter = $json }
--- provision-sharepointsite-koksmat/sync-github-org.ps1
+++ provision-sharepointsite-koksmat/sync-github-org.ps1
@@ -1,12 +1,8 @@
 $ErrorActionPreference = "Stop"
 
-# try {
-
-$site = get-pnpsite
-#List "Repositories"
-  
-$orgName = "nexi-intra"
-
+$orgName = $env:GITHUB_ORG
+$site = Get-Pnpsite
+write-host "Syncing repositories for organization: $orgName with site $($site.Url) )"
 $repos = gh repo list $orgName --json "name,visibility" --limit 999 | ConvertFrom-Json
 
 $repoDictionary = @{}
@@ -23,32 +19,44 @@ foreach ($repo in $repos) {
 }
 
 # Retrieve all items from the SharePoint list "Repositories"
-$listItems = Get-PnPListItem -List "Repositories"
-
-# Iterate through SharePoint list items and compare with the dictionary
-foreach ($item in $listItems) {
-  # Construct the key from SharePoint item fields
-  $listOrgName = $item.FieldValues["OrganisationName"]  # Replace with your actual SharePoint field name
-  $listRepoName = $item.FieldValues["RepositoryName"]  # Replace with your actual SharePoint field name
-  $key = "$listOrgName/$listRepoName"
-
-  if ($repoDictionary.ContainsKey($key)) {
-    Write-Host "Match found for key: $key" -ForegroundColor Green
-  }
-  else {
-    Write-Host "No match found for key: $key in the dictionary" -ForegroundColor Red
-  }
+$listItems = Get-PnPList "Repositories" -ErrorAction SilentlyContinue
+if ($null -eq $listItems) {
+  Write-Host "Missing list Repositories" -ForegroundColor Red
+  throw "Sync repos failed - Missing list Repositories"
+  return
 }
 
+# Iterate through SharePoint list items and compare with the dictionary
+if ($listItems.ItemCount -ne 0) {
+  
+
+  foreach ($item in $listItems) {
+    # Construct the key from SharePoint item fields
+    $listOrgName = $item.FieldValues["OrganisationName"]  # Replace with your actual SharePoint field name
+    $listRepoName = $item.FieldValues["RepositoryName"]  # Replace with your actual SharePoint field name
+    $key = "$listOrgName/$listRepoName"
+
+    if ($repoDictionary.ContainsKey($key)) {
+      Write-Host "Match found for key: $key" -ForegroundColor Green
+    }
+    else {
+      Write-Host "No match found for key: $key in the dictionary" -ForegroundColor Red
+    }
+  }
+}
 # Check for repositories in the dictionary that are not in the SharePoint list
 foreach ($key in $repoDictionary.Keys) {
-  $found = $listItems | Where-Object {
-    "$($_.FieldValues["OrganisationName"])/$($_.FieldValues["RepositoryName"])" -eq $key
+  $found = $false
+  if ($listItems.ItemCount -gt 0) {
+   
+    $found = $listItems | Where-Object {
+      "$($_.FieldValues["OrganisationName"])/$($_.FieldValues["RepositoryName"])" -eq $key
+    }
   }
   if (-not $found) {
     Write-Host "Repository in dictionary but not in SharePoint: $key" -ForegroundColor Yellow
     Add-PnPListItem -List "Repositories" -Values @{
-      "Title"            = $key
+      "Title"            = $repoDictionary[$key].name + " ($orgName)"
       "OrganisationName" = $orgName
       "RepositoryName"   = $repoDictionary[$key].name
       "RepositoryType"   = $repoDictionary[$key].visibility
@@ -57,12 +65,3 @@ foreach ($key in $repoDictionary.Keys) {
 }
   
   
-  
-
-  
-    
-# }
-# catch {
-#   Write-Host "Error: $_" -ForegroundColor Red
-#   throw "Sync repos failed - Error: $_"
-# }
